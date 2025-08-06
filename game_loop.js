10f0fae2e44de463d7b0974023a6f6519b98925c
Eucatastrophe <108638101+makrcat@users.noreply.github.com>
2025-08-05 22:45:56 -0700

    somehow I named the file wrong

--- game_loop.js
+++ game_loop.js
@@ -1,35 +1,61 @@
 //@ts-check
 
-import { GameCanvas } from "./game_engine.js";
-import { gameLoop } from "./game_config.js";
+import { GameCanvas, isKeyDown } from "./game_engine.js";
+import { Rect, Circle, Line } from "./game_engine.js";
 
-let fps = 60;
-let now;
-let then = Date.now();
-let interval = 1000/fps;
-let delta;
 const canvas = document.getElementById("canvas");
-
 const gameCanvas = new GameCanvas(canvas);
+
+///////////////////define variables/////////////////////
+let square = new Rect(gameCanvas.ctx, 10, 10, 50, 50, "purple");
+square.fill = false;
+square.borderColor = "purple";
+square.border = true;
+square.lineWidth = 2;
+
+let circle = new Circle(gameCanvas.ctx, 100, 100, 20, "red");
+circle.fill = true;
+
+let line = new Line(gameCanvas.ctx);
+
+document.onmousemove = updateCoordinates;
+let clientX = 0;
+let clientY = 0;
+
+function updateCoordinates(e) {
+  clientX = e.clientX;
+  clientY = e.clientY;
+  console.log("Mouse coordinates", clientX, clientY);
+}
 
 //////////////////game!////////////////////////////////
 
-function startGame() {
-  //clear(ctx);
-  
-  requestAnimationFrame(startGame);
-  now = Date.now();
-  delta = now - then;
+export function gameLoop() {
+  circle.centerX = clientX;
+  circle.centerY = clientY;
 
-  if (delta > interval) {
-    then = now - (delta % interval);
-
-    gameCanvas.clear();
-    gameCanvas.updateFrame();
-
-    gameLoop();
+  if ((isKeyDown["ArrowLeft"] || isKeyDown["a"]) && square.leftEdge > 0) {
+    square.x -= 5;
   }
+
+  //@ts-ignore
+  if ((isKeyDown["ArrowRight"] || isKeyDown["d"]) && square.rightEdge < canvas.width) {
+    square.x += 5;
+  }
+
+  if ((isKeyDown["ArrowUp"] || isKeyDown["w"]) && square.topEdge > 0) {
+    square.y -= 5;
+  }
+
+  //@ts-ignore
+  if ((isKeyDown["ArrowDown"] || isKeyDown["s"]) && square.bottomEdge < canvas.height) {
+    square.y += 5;
+  }
+
+  line.x2 = square.x + square.width / 2;
+  line.y2 = square.y + square.height / 2;
+  line.x1 = circle.centerX;
+  line.y1 = circle.centerY;
+
+  console.log(square.leftEdge, square.rightEdge, square.topEdge, square.bottomEdge);
 }
-
-
-startGame()
